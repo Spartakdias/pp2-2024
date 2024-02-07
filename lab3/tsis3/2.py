2550f195c2120dc34366e3d94fbd36adcbf82bf5
Spartakdias <d.kadyraly@gmail.com>
2024-02-07 10:26:13 +0600

    lab3

---
+++ lab3/tsis3/2.py
@@ -0,0 +1,22 @@
+class Shape:
+    def __init__(self):
+        pass
+
+    def area(self):
+        return 0
+
+class Square(Shape):
+    def __init__(self, length):
+        super().__init__()
+        self.length = length
+
+    def area(self):
+        return self.length ** 2
+
+a = int(input())
+if __name__ == "__main__":
+    shape = Shape()
+    square = Square(a)
+
+    print(shape.area())  
+    print( square.area())  
---
+++ lab3/tsis3/3.py
@@ -0,0 +1,22 @@
+class Shape:
+    def __init__(self):
+        pass
+
+    def area(self):
+        return 0
+    
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        super().__init__()
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+a = int(input())
+b = int(input())
+if __name__ == "__main__":
+    rectangle = Rectangle(a, b)
+
+    print( rectangle.area())  
