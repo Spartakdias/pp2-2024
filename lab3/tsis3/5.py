cc9703c82c62d006869a6f25058122e784171ff0
Spartakdias <d.kadyraly@gmail.com>
2024-02-09 20:23:17 +0600

    lab3

---
+++ lab3/tsis3/5.py
@@ -0,0 +1,27 @@
+class BankAccount:
+    def __init__(self, owner, initial_balance=0):
+        self.owner = owner
+        self.balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Пополнение на {amount} тенге выполнено. Данный баланс: {self.balance} тенге")
+        else:
+            print("Ошибка: Сумма пополнения должна быть положительной.")
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            print(f"Снятие {amount} тенге выполнено. Данный баланс: {self.balance} тенге")
+        else:
+            print("Ошибка: Недостаточно средств для снятия или сумма снятия некорректна.")
+
+a = input("Name: ")
+b = int(input("Money = "))
+if __name__ == "__main__":
+    account = BankAccount(a, b)
+    c = int(input())
+    d = int(input())
+    account.deposit(c) 
+    account.withdraw(d)  
